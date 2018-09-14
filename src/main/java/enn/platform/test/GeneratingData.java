343ccdbf631eed8905a1079635f71e5f82d84b0e
chenchen <altenchen@aliyun.com>
2018-09-14 10:05:30 +0800

    修复结束时间不准的bug

---
+++ src/main/java/enn/platform/test/GeneratingData.java
@@ -0,0 +1,11 @@
+package enn.platform.test;
+
+/**
+ * @Author:chenchen
+ * @Description:
+ * @Date:2018/9/13
+ * @Project:opentsdbtest
+ * @Package:enn.platform.test
+ */
+public class GeneratingData {
+}
